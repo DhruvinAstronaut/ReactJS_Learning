0358aa4b7acd5ea5b40d67306feb771dba16c927
SOTSYS141 <sotsys141@SOTSYS141s-Mac-mini.local>
2022-09-02 15:39:35 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
-import Counter from "./components/Counter";
+import Counter from "./components/Counter"; 
 import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
